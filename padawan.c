b824b0675aa06ab4f90cb5eb6d005fe6dc673f53
Lucio Tamborini <59577462+luchimbo@users.noreply.github.com>
2020-06-25 09:46:31 -0300

    Add files via upload

--- padawan.c
+++ padawan.c
@@ -199,6 +199,5 @@ printf("En %f meses se convertira en jedi\n ", tiempo_jedi);
 if (probabilidad_convertirse > 85)
 printf("Transmite lo que has aprendido: fuerza, maestria; pero insensatez, debilidad, fracaso tambien. ¡Si, fracaso sobre todo! El mejor profesor, el fracaso es.\n By Master Yoda\n"); 
 
-
 return 0;
 }
